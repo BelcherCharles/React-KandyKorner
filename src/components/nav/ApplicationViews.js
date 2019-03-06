c7122e9980a81a512ee7ad2f55b4d763aa3095d9
Charles Belcher <belchercharles@yahoo.com>
2019-03-06 15:08:38 -0500

    adding routing

--- src/components/nav/ApplicationViews.js
+++ src/components/nav/ApplicationViews.js
@@ -6,43 +6,70 @@ import EmployeeList from '../EmployeeList/employeeList'
 
 
 class ApplicationViews extends Component {
-    StoreLocations = [
-        { id: 1, name: "Store 1", address: "123 Somewhere St" },
-        { id: 2, name: "Store 2", address: "45 St. Elsewhere Rd." },
-        { id: 3, name: "Store 3", address: "21 Jump Street" }
-    ]
-    Employees = [
-        { id: 1, name: "Freddy K." },
-        { id: 2, name: "Jason V." },
-        { id: 3, name: "PennyWise" },
-        { id: 4, name: "Predator" }
-    ]
-    CandyTypes = [
-        { id: 1, name: "Gummy shits" },
-        { id: 2, name: "Suckers for suckers" },
-        { id: 3, name: "Sugar-Free Trash" },
-        { id: 4, name: "Choco Overload" }
-    ]
-    Candies = [
-        { id: 1, typeId: 1, name: "Gummy Worms" },
-        { id: 2, typeId: 1, name: "Gummy Bears" },
-        { id: 3, typeId: 1, name: "Sour Patch Kids" },
-        { id: 4, typeId: 2, name: "Dum-Dums" },
-        { id: 5, typeId: 2, name: "Blow-Pops" },
-        { id: 6, typeId: 2, name: "Tootsie-Pops" },
-        { id: 7, typeId: 3, name: "Salt-Water Taffy" },
-        { id: 8, typeId: 4, name: "Reese's Pieces" },
-        { id: 9, typeId: 4, name: "M&Ms" },
-        { id: 10, typeId: 4, name: "Hershey Kisses" }
-    ]
+    // StoreLocations = [
+    //     { id: 1, name: "Store 1", address: "123 Somewhere St" },
+    //     { id: 2, name: "Store 2", address: "45 St. Elsewhere Rd." },
+    //     { id: 3, name: "Store 3", address: "21 Jump Street" }
+    // ]
+    // Employees = [
+    //     { id: 1, name: "Freddy K." },
+    //     { id: 2, name: "Jason V." },
+    //     { id: 3, name: "PennyWise" },
+    //     { id: 4, name: "Predator" }
+    // ]
+    // CandyTypes = [
+    //     { id: 1, name: "Gummy shits" },
+    //     { id: 2, name: "Suckers for suckers" },
+    //     { id: 3, name: "Sugar-Free Trash" },
+    //     { id: 4, name: "Choco Overload" }
+    // ]
+    // Candies = [
+    //     { id: 1, typeId: 1, name: "Gummy Worms" },
+    //     { id: 2, typeId: 1, name: "Gummy Bears" },
+    //     { id: 3, typeId: 1, name: "Sour Patch Kids" },
+    //     { id: 4, typeId: 2, name: "Dum-Dums" },
+    //     { id: 5, typeId: 2, name: "Blow-Pops" },
+    //     { id: 6, typeId: 2, name: "Tootsie-Pops" },
+    //     { id: 7, typeId: 3, name: "Salt-Water Taffy" },
+    //     { id: 8, typeId: 4, name: "Reese's Pieces" },
+    //     { id: 9, typeId: 4, name: "M&Ms" },
+    //     { id: 10, typeId: 4, name: "Hershey Kisses" }
+    // ]
 
 
     state = {
-        stores: this.StoreLocations,
-        employees: this.Employees,
-        candyTypes: this.CandyTypes,
-        candies: this.Candies,
+        stores: [],
+        employees: [],
+        candyTypes: [],
+        candies: []
     }
+
+    componentDidMount() {
+        const newState = {};
+        fetch("http://localhost:5002/Employees")
+            .then(employees => employees.json())
+            .then(parsedEmployees => {
+                // console.log(parsedEmployees);
+                // this.setState({employees: parsedEmployees})
+                newState.employees = parsedEmployees;
+                return fetch("http://localhost:5002/StoreLocations")
+            })
+            .then(stores => stores.json())
+            .then(parsedStores => {
+                newState.stores = parsedStores;
+                return fetch("http://localhost:5002/Candies")
+            })
+            .then(candies => candies.json())
+            .then(parsedCandies => {
+                newState.candies = parsedCandies;
+                return fetch("http://localhost:5002/CandyTypes")
+            })
+                .then(types => types.json())
+                .then(parsedTypes => {
+                    newState.candyTypes = parsedTypes
+                    this.setState(newState)
+            })
+        }
 
     render() {
         return (
